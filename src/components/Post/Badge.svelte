68f7ae4e04b82d184fc588f263f3ca6cc72b508b
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-06-19 15:17:26 +0100

    Add group names to badges

--- src/components/Post/Badge.svelte
+++ src/components/Post/Badge.svelte
@@ -7,32 +7,57 @@
 		| 'wiki-specialist'
 		| 'soap'
 		| 'global-discussions-moderator';
+
+	type GroupInfo = {
+		name: string;
+		url: string;
+	};
 </script>
 
 <script lang="ts">
 	// Stolen from https://dev.fandom.com/wiki/MediaWiki:UserBadge/code.js
 	// prettier-ignore
-	var BADGES_DEFAULTS :Record<BadgeName, string>= {
-		// local
-		'sysop':                        'https://vignette.wikia.nocookie.net/central/images/1/12/Badge-Admin.svg',
-		'content-moderator':            'https://vignette.wikia.nocookie.net/central/images/e/ef/Badge-ContentModerator.svg',
-		'threadmoderator':              'https://vignette.wikia.nocookie.net/central/images/5/50/Badge-DiscussionsModerator.svg',
+	var BADGES_DEFAULTS: Record<BadgeName, GroupInfo> = {
+		'sysop': {
+			name: 'Sysop',
+			url: 'https://vignette.wikia.nocookie.net/central/images/1/12/Badge-Admin.svg',
+		},
+		'content-moderator': {
+			name: 'Content Moderator',
+			url: 'https://vignette.wikia.nocookie.net/central/images/e/ef/Badge-ContentModerator.svg',
+		},
+		'threadmoderator': {
+			name: 'Thread Moderator',
+			url: 'https://vignette.wikia.nocookie.net/central/images/5/50/Badge-DiscussionsModerator.svg',
+		},
 		// global
-		'staff':                        'https://vignette.wikia.nocookie.net/central/images/0/06/Badge-Staff.svg',
-		'wiki-specialist':              'https://vignette.wikia.nocookie.net/central/images/0/06/Badge-Staff.svg',
-		'soap':                         'https://vignette.wikia.nocookie.net/central/images/9/9a/Badge-SOAP.svg',
-		'global-discussions-moderator': 'https://vignette.wikia.nocookie.net/central/images/4/40/Badge-GlobalDiscussionsModerator.svg',
+		'staff': {
+			name: 'Staff',
+			url: 'https://vignette.wikia.nocookie.net/central/images/0/06/Badge-Staff.svg',
+		},
+		'wiki-specialist': {
+			name: 'Wiki Specialist',
+			url: 'https://vignette.wikia.nocookie.net/central/images/0/06/Badge-Staff.svg',
+		},
+		'soap': {
+			name: 'SOAP',
+			url: 'https://vignette.wikia.nocookie.net/central/images/9/9a/Badge-SOAP.svg',
+		},
+		'global-discussions-moderator': {
+			name: 'Global Discussions Moderator',
+			url: 'https://vignette.wikia.nocookie.net/central/images/4/40/Badge-GlobalDiscussionsModerator.svg',
+		},
 	};
 
 	export let group: BadgeName;
 
 	async function getBadge(badgeName: BadgeName) {
 		const img = document.createElement('img');
-		img.src = BADGES_DEFAULTS[badgeName];
+		img.src = BADGES_DEFAULTS[badgeName].url;
 		img.style.width = '1em';
 		img.style.height = '1em';
-		img.alt = badgeName;
-		img.title = badgeName;
+		img.alt = BADGES_DEFAULTS[badgeName].name;
+		img.title = BADGES_DEFAULTS[badgeName].name;
 
 		return img.outerHTML;
 	}
