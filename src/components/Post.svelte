de7e6662e82b041568c1b9c75106afb7d0dc8ff8
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-06-19 15:35:01 +0100

    Add username to user info

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -6,7 +6,46 @@
 	export let post: Post;
 </script>
 
-<container>
-	<Avatar user={post.createdBy} />
+<div class="post-container">
+	<div class="user-info">
+		<Avatar user={post.createdBy} />
+		<a class="user-link" href={'/f/u/' + post.createdBy.id}>{post.createdBy.name}</a>
+	</div>
 	<PostBody jsonModel={post.jsonModel} attachments={post._embedded.attachments[0]} />
-</container>
+</div>
+
+<style>
+	.post-container {
+		color: var(--theme-page-text-color);
+	}
+
+	.user-info {
+		display: flex;
+		padding: 5px;
+	}
+
+	.user-info :global(.avatar) {
+		width: 60px;
+	}
+
+	.user-link:hover {
+		color: var(--theme-page-text-color--hover);
+	}
+
+	.user-link {
+		color: var(--theme-page-text-color);
+		display: inline-block;
+		font-weight: 700;
+		max-width: 16em;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		transition: color 0.3s;
+		/* vertical-align: middle; */
+		align-content: center;
+		white-space: nowrap;
+	}
+
+	a {
+		text-decoration: none;
+	}
+</style>
