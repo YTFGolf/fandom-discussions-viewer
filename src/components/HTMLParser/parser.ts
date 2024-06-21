200f0d0eae8f91e04857fe9e3e83f02703401fd6
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-06-21 21:30:20 +0100

    Refactor parse function

--- src/components/HTMLParser/parser.ts
+++ src/components/HTMLParser/parser.ts
@@ -1,20 +1,38 @@
 import type { Attachments } from '$lib/controllers/types/attachments';
 import type { DocModel } from '$lib/controllers/types/jsonModel';
 import parseBlock from './block';
+import PostBody from '../Post/PostBody.svelte';
 
-export default async function parse(
-	container: HTMLDivElement,
-	attachments: Attachments,
-): Promise<[string, DocModel]> {
-	const rawContent = container.innerText;
+async function getRawContent(container: HTMLDivElement) {
 	// FIXME also contains the text of the link in OpenGraphs
+	// should probably be just based on the text elements of post.
+	// or I can change visibility lmao
+	return container.innerText;
+}
 
+async function getPost(container: HTMLDivElement, attachments: Attachments) {
 	const postContent = [];
 	for (const child of container.children as any as HTMLElement[]) {
 		postContent.push(parseBlock(child, attachments));
 	}
 
 	const post: DocModel = { content: await Promise.all(postContent) };
+	return post;
+}
 
-	return [rawContent, post];
+/**
+ * Note: this function assumes that the container is well-formed (i.e. no
+ * `Malformed content` or weird API-only stuff).
+ * @param container The output of {@link PostBody}.
+ * @return `[rawContent, jsonModel]`
+ */
+export default async function parse(
+	container: HTMLDivElement,
+	attachments: Attachments,
+): Promise<[string, DocModel]> {
+	const rawContent = getRawContent(container);
+
+	const post = getPost(container, attachments);
+
+	return Promise.all([rawContent, post]);
 }
