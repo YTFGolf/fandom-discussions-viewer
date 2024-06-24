6cb2a65f7b532dfc9ebb2e63b14ad63ee50039ca
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-06-24 12:52:58 +0100

    Add base switcher component

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
-	import { canSplit } from 'prosemirror-transform';
 	import type { Attachments } from '$lib/controllers/types/attachments';
 	import { DiscussionPost } from '$lib/controllers/wikia/DiscussionPost';
 	import { examples } from '../routes/f/p/[...postParams=forumPost]/examples';
-	import { keymap } from 'prosemirror-keymap';
-	import { EditorState, NodeSelection, Transaction, type Command } from 'prosemirror-state';
-	import { EditorView } from 'prosemirror-view';
-	import { DOMParser, Fragment, NodeType, Slice, type Attrs } from 'prosemirror-model';
-	import { baseKeymap, chainCommands, toggleMark } from 'prosemirror-commands';
-	import { history, redo, undo } from 'prosemirror-history';
 	import { onMount } from 'svelte';
-	import { schema } from './Editor/schema';
-	import { getMenu } from './Editor/Menu';
 	import type { UserDetails } from '$lib/responses/Post';
 	import Avatar from './Post/Avatar.svelte';
+	import { JSONView, ProseMirrorView, type View } from './Editor/Switcher';
 
 	/**
 	 * Plan: This takes in a few props: rawContent, JSONModel, Attachments. All
@@ -102,90 +94,12 @@
 	// 	return rawContent.join('\n');
 	// }
 
-	// https://github.com/ProseMirror/prosemirror-schema-list/blob/79b62755a061869e0dfa1e877b5033f341dc0592/src/schema-list.ts#L114
-	export function splitListItem(itemType: NodeType, itemAttrs?: Attrs): Command {
-		return function (state: EditorState, dispatch?: (tr: Transaction) => void) {
-			let { $from, $to, node } = state.selection as NodeSelection;
-			if ((node && node.isBlock) || $from.depth < 3 || !$from.sameParent($to)) return false;
-			let grandParent = $from.node(-2);
-			if (grandParent.type != itemType) return false;
-			if ($from.parent.content.size == 0 && $from.node(-1).childCount == $from.indexAfter(-1)) {
-				// In an empty block. If this is a nested list, the wrapping
-				// list item should be split. Otherwise, bail out and let next
-				// command handle lifting.
-				if (
-					$from.depth == 3 ||
-					$from.node(-3).type != itemType ||
-					$from.index(-2) != $from.node(-2).childCount - 1
-				)
-					return false;
-				if (dispatch) {
-					let wrap = Fragment.empty;
-					let depthBefore = $from.index(-1) ? 1 : $from.index(-2) ? 2 : 3;
-					// Build a fragment containing empty versions of the structure
-					// from the outer list item to the parent node of the cursor
-					for (let d = $from.depth - depthBefore; d >= $from.depth - 3; d--)
-						wrap = Fragment.from($from.node(d).copy(wrap));
-					let depthAfter =
-						$from.indexAfter(-1) < $from.node(-2).childCount
-							? 1
-							: $from.indexAfter(-2) < $from.node(-3).childCount
-								? 2
-								: 3;
-					// Add a second list item with an empty default start node
-					wrap = wrap.append(Fragment.from(itemType.createAndFill()));
-					let start = $from.before($from.depth - (depthBefore - 1));
-					let tr = state.tr.replace(
-						start,
-						$from.after(-depthAfter),
-						new Slice(wrap, 4 - depthBefore, 0),
-					);
-					let sel = -1;
-					tr.doc.nodesBetween(start, tr.doc.content.size, (node, pos) => {
-						if (sel > -1) return false;
-						if (node.isTextblock && node.content.size == 0) sel = pos + 1;
-					});
-					if (sel > -1) tr.setSelection(Selection.near(tr.doc.resolve(sel)));
-					dispatch(tr.scrollIntoView());
-				}
-				return true;
-			}
-			let nextType = null;
-			let tr = state.tr.delete($from.pos, $to.pos);
-			let types = nextType
-				? [itemAttrs ? { type: itemType, attrs: itemAttrs } : null, { type: nextType }]
-				: undefined;
-			if (!canSplit(tr.doc, $from.pos, 2, types)) return false;
-			if (dispatch) dispatch(tr.split($from.pos, 2, types).scrollIntoView());
-			return true;
-		};
-	}
-
-	let editorView: EditorView;
-	let content: HTMLElement;
+	let editorView: View;
 	let editor: HTMLElement;
+	let content: HTMLElement;
 	let isLoaded = false;
 	onMount(() => {
-		editorView = new EditorView(editor, {
-			state: EditorState.create({
-				doc: DOMParser.fromSchema(schema).parse(content),
-				plugins: [
-					history(),
-					keymap({
-						'Mod-z': undo,
-						'Mod-y': redo,
-						'Mod-b': toggleMark(schema.marks.strong),
-						'Mod-i': toggleMark(schema.marks.em),
-						Enter: chainCommands(
-							splitListItem(schema.nodes.bulletList),
-							splitListItem(schema.nodes.orderedList),
-						),
-					}),
-					getMenu(),
-					keymap(baseKeymap),
-				],
-			}),
-		});
+		editorView = new ProseMirrorView(editor, content);
 
 		content.remove();
 		isLoaded = true;
@@ -194,14 +108,17 @@
 	let model = '';
 
 	const logDocumentModel = () => {
-		const dom = editorView.dom;
-		const doc = editorView.state.doc;
-		console.log(dom.children);
-		console.log(doc.toJSON());
-		console.log(doc);
-
-		model = JSON.stringify(doc.toJSON(), undefined, '    ');
+		model = JSON.stringify(editorView.content, undefined, '    ');
+		// editorView.destroy();
 	};
+
+	function switchEditor(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
+		logDocumentModel();
+		// model = JSON.stringify(editorView.content, undefined, '    ');
+		// editorView.destroy();
+		// editorView = new JSONView(editor, model);
+		// console.log(event);
+	}
 
 	const userDetails: UserDetails = {
 		id: '27706221',
@@ -218,7 +135,10 @@
 		<Avatar user={userDetails} />
 		<div id="editor" data-placeholder="Share your thoughts…" bind:this={editor}></div>
 	</div>
-	<div class="form-actions"><button on:click={logDocumentModel}>Submit</button></div>
+	<div class="form-actions">
+		<!-- <button on:click={logDocumentModel}>Submit</button> -->
+		<button on:click={switchEditor}>Switch Editor</button>
+	</div>
 </div>
 <div class="fallback" style={isLoaded ? 'display: none' : ''}>Loading editor...</div>
 <pre>{model}</pre>
@@ -293,6 +213,11 @@
 	#editor :global(.menubar .menuicon.active) {
 		background: rgba(var(--webeditor-link-color--rgb), 0.15);
 		color: var(--webeditor-link-color);
+	}
+
+	#editor :global(.menubar .menuicon.disabled) {
+		background: rgba(var(--webeditor-link-color--rgb), 0) !important;
+		color: grey;
 	}
 
 	.form-content {
