8cdf8df14252c8dbf62e4d707c753c9706377bc8
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-06-26 12:53:47 +0100

    Make theme styles dynamically loaded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,26 @@
-<script>
+<script lang="ts">
+	import type { Wiki } from '$lib/types';
 	import Header from './Header.svelte';
+
+	const wiki: Wiki = { name: 'wwr-test', lang: 'en' };
+	const theme = 'light';
+	let entrypoint = `https://${wiki.name}.fandom.com`;
+
+	if (wiki.lang && wiki.lang !== 'en') {
+		entrypoint += '/' + wiki.lang;
+	}
+	// TODO make this a config option
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="/wwr-theme-variables.css" />
+	<link
+		rel="stylesheet"
+		href="{entrypoint}/wikia.php?controller=ThemeApi&method=themeVariables&variant={theme}"
+	/>
+	<link
+		rel="stylesheet"
+		href="{entrypoint}/resources-ucp/mw139/dist/styles/ext.fandom.GlobalComponents.GlobalComponentsTheme.{theme}.css"
+	/>
 	<link rel="stylesheet" href="/styles.css" />
 </svelte:head>
 
