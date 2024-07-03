75a41d561f538f798a8e3cee83fa4fb202e1429e
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-07-03 21:37:27 +0100

    Add navigation to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,11 @@
 </svelte:head>
 
 <h1>Home</h1>
+<p>Pages:</p>
+<ul>
+	<li><a href="/login">Login</a></li>
+	<li><a href="/f/p/4400000000000822918?wiki=en.battle-cats">Store</a></li>
+	<li><a href="/settings">Settings</a> (also in the gear at the top)</li>
+	<li><a href="/test">Test</a> (basically unused)</li>
+	<li><a href="/demo">Demo</a> (deprecated)</li>
+</ul>
