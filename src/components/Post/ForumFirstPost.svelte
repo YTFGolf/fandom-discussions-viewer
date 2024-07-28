7a5a34a6239bb22d8a85b52bf750eea6416862c9
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-07-28 18:43:31 +0100

    Add image answers

--- src/components/Post/ForumFirstPost.svelte
+++ src/components/Post/ForumFirstPost.svelte
@@ -10,6 +10,8 @@
 	export let thread: Thread;
 	let poll: Poll | undefined;
 	$: poll = thread._embedded.attachments[0].polls?.[0];
+	let isImages: boolean;
+	$: isImages = poll?.answers[0].image ? true : false;
 
 	// gonna need to let it redefine what it means to be a post
 	// gonna neeed another component for this, gonna need to make components
@@ -72,17 +74,15 @@
 
 	let currentAnswer: HTMLButtonElement;
 	function selectAnswer(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
-		if (event.target == currentAnswer) {
+		if (event.currentTarget == currentAnswer) {
 			return;
 		}
 		if (currentAnswer) {
 			currentAnswer.classList.remove('is-selected');
 		}
-		console.log('a');
 
-		const target = event.target as HTMLButtonElement;
-		target.classList.add('is-selected');
-		currentAnswer = target;
+		event.currentTarget.classList.add('is-selected');
+		currentAnswer = event.currentTarget;
 	}
 </script>
 
@@ -96,17 +96,46 @@
 />
 {#if poll}
 	<h4 class="poll-question">{poll.question}</h4>
-	{#each poll.answers as answer}
-		{JSON.stringify(answer)}
-		<button class="poll-answer" on:click={selectAnswer}>{answer.text}</button>
-	{/each}
-	{#if false}
-		<button class="poll-answer is-selected" style="display:none"></button>
-	{/if}
+	{JSON.stringify(poll.answers)}
+	<div class="poll-answers {isImages ? 'image-poll' : 'text-poll'} items-{poll.answers.length}">
+		{#each poll.answers as answer}
+			{#if answer.image}
+				<button class="poll-answer" on:click={selectAnswer}>
+					<div class="answer-image-wrapper">
+						<img
+							src={answer.image.url}
+							alt={answer.text}
+							class="poll-answer-image"
+							sizes="(max-width: 420px) 180px, 370px"
+						/>
+					</div>
+					<span class="answer-text">{answer.text}</span>
+				</button>
+			{:else}
+				<button class="poll-answer" on:click={selectAnswer}>{answer.text}</button>
+			{/if}
+		{/each}
+		{#if false}
+			<button class="poll-answer is-selected" style="display:none"></button>
+		{/if}
+	</div>
 {/if}
 
 <style>
-	.poll-answer {
+	.poll-answers.image-poll {
+		grid-column-gap: 13px;
+		grid-row-gap: 13px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		margin-bottom: 14px;
+		max-width: 750px;
+	}
+
+	.poll-answers.image-poll.items-6 {
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+
+	.text-poll .poll-answer {
 		width: -moz-available;
 		background-color: inherit;
 
@@ -126,14 +155,78 @@
 			border-color 0.3s;
 	}
 
-	.poll-answer:hover,
-	.poll-answer.is-selected {
+	.text-poll .poll-answer:hover,
+	.text-poll .poll-answer.is-selected {
 		border-color: var(--theme-link-color);
 	}
 
-	.poll-answer.is-selected {
+	.text-poll .poll-answer.is-selected {
 		border-width: 3px;
 		font-weight: 600;
 		padding: 10px 10px 8px;
+	}
+
+	.image-poll .poll-answer {
+		border-radius: 5px;
+		box-shadow: 0 0 0 1px var(--theme-border-color);
+		color: #fff;
+		cursor: pointer;
+		overflow: hidden;
+		position: relative;
+		text-align: center;
+		transition: box-shadow 0.3s;
+		border: none;
+	}
+
+	.image-poll .poll-answer:hover {
+		box-shadow: 0 0 0 1px var(--theme-link-color);
+	}
+
+	.image-poll .poll-answer.is-selected {
+		box-shadow: 0 0 0 3px var(--theme-link-color);
+		font-weight: 500;
+	}
+
+	.image-poll .answer-image-wrapper {
+		height: 0;
+		padding-bottom: 100%;
+		width: 100%;
+	}
+
+	.image-poll .poll-answer-image {
+		height: 100%;
+		left: 0;
+		position: absolute;
+		top: 0;
+		width: 100%;
+		-o-object-fit: cover;
+		object-fit: cover;
+	}
+
+	.image-poll .answer-image-wrapper::after {
+		height: 100%;
+		left: 0;
+		position: absolute;
+		top: 0;
+		width: 100%;
+		background-image: linear-gradient(180deg, transparent, #222);
+		content: '';
+	}
+
+	.image-poll .answer-text {
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 3;
+		bottom: 20px;
+		display: -webkit-box;
+		font-size: 18px;
+		left: 0;
+		line-height: 1.25;
+		overflow: hidden;
+		padding: 0 10px;
+		position: absolute;
+		text-overflow: ellipsis;
+		width: 100%;
+		word-break: break-word;
+		font-weight: bold;
 	}
 </style>
