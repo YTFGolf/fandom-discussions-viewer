3dfe466e767c56535e87232bd4dbc2617d70bfd4
YTFGolf <77898443+YTFGolf@users.noreply.github.com>
2024-07-31 12:05:22 +0100

    Remove unnecessary code in ForumFirstPost

--- src/components/Post/ForumFirstPost.svelte
+++ src/components/Post/ForumFirstPost.svelte
@@ -20,17 +20,6 @@
 	let poll: ThreadPoll | undefined;
 	$: poll = thread._embedded.attachments[0].polls?.[0];
 
-	// gonna need to let it redefine what it means to be a post
-	// gonna neeed another component for this, gonna need to make components
-	// like ForumPost and ForumFirstPost as well as base Post
-
-	// base post deals with stuff like display, while ForumPost deals with all
-	// the functions
-
-	function getNewAnswer(answer: ThreadPollAnswer): SendPollAnswer {
-		return { ...answer, id: undefined, votes: undefined } as SendPollAnswer;
-	}
-
 	async function deleteFunction(wiki: Wiki, thread: Thread) {
 		return DiscussionThread.deleteThread(wiki, { threadId: thread.id });
 		// return DiscussionPost.deletePost(wiki, { postId: thread.firstPostId });
@@ -38,18 +27,15 @@
 		// post as deleted
 	}
 	async function undeleteFunction(wiki: Wiki, thread: Thread) {
-		return DiscussionThread.undelete(wiki, {
-			threadId: (thread as any as Post).threadId || thread.id,
-		});
+		return DiscussionThread.undelete(wiki, { threadId: thread.id });
 	}
 	async function updateFunction(wiki: Wiki, thread: Thread, data: EditorContent) {
 		let poll: SendPoll | undefined = undefined;
 		if (thread.poll) {
 			poll = {
-				...thread.poll,
-				totalVotes: undefined,
-				userVotes: undefined,
-				answers: thread.poll.answers.map(getNewAnswer),
+				id: thread.poll.id,
+				question: thread.poll.question,
+				answers: [],
 			} as SendPoll;
 			data.attachments.polls = [poll as SendPoll] as any;
 		}
@@ -102,7 +88,7 @@
 			// @ts-ignore
 			_embedded: undefined,
 		};
-		console.log(JSON.stringify(newPollData));
+
 		const res = await DiscussionThread.update($wiki, { threadId: thread.id }, update);
 		if (res.status === HTTP.OK) {
 			const thread = (await res.json()) as Thread;
